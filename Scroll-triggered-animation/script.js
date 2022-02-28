146a902a5376166320691d7499b66a33b5eb2339
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-02-28 21:44:35 +0530

    Create script.js

---
+++ Scroll-triggered-animation/script.js
@@ -0,0 +1,40 @@
+function scrollTrigger(selector, options = {}){
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+        addObserver(el, options)
+    })
+}
+
+function addObserver(el, options){
+    if(!('IntersectionObserver' in window)){
+        if(options.cb){
+            options.cb(el)
+        }else{
+            entry.target.classList.add('active')
+        }
+        return
+    }
+    let observer = new IntersectionObserver((entries, observer) => { //this takes a callback function which receives two arguments: the elemts list and the observer instance
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                if(options.cb){
+                    options.cb(el)
+                }else{
+                    entry.target.classList.add('active')
+                }
+                observer.unobserve(entry.target)
+            }
+        })
+    }, options)
+    observer.observe(el)
+}
+// Example usages:
+scrollTrigger('.intro-text')
+
+scrollTrigger('.scroll-reveal', {
+    rootMargin: '-200px',
+})
+
+scrollTrigger('.loader', {
+    ro
